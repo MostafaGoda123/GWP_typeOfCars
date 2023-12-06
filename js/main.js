f4d67091bc4dfac12ac5936ddb678e751fe41653
Eng\ Mostafa El-Sayed <104257870+MostafaGoda123@users.noreply.github.com>
2023-12-06 13:31:41 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+let types = document.querySelectorAll(".type .image");
+let photos = document.querySelectorAll(".photo .image");
+types.forEach(img => {
+   img.addEventListener("click",function () {
+   types.forEach(img => {
+      img.classList.remove("active");
+   })
+   photos.forEach(ph => {
+      ph.classList.remove("active");
+   })
+   img.classList.add("active");
+   photos.forEach(ph => {
+      if (img.getAttribute("data-name") == ph.getAttribute("data-name"))
+      {
+         ph.classList.add("active");
+      }
+   })
+   })
+})
+
+
+
+
+
