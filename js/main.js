27076a32d8a369d35d5d8f46721ee768f88bf688
Eng\ Mostafa El-Sayed <104257870+MostafaGoda123@users.noreply.github.com>
2023-12-09 14:31:50 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,24 +1,20 @@
-let types = document.querySelectorAll(".type .image");
-let photos = document.querySelectorAll(".photo .image");
-types.forEach(img => {
+let types = document.querySelectorAll(".type .image")
+let photos = document.querySelectorAll(".photo .image")
+
+types.forEach(img =>{
    img.addEventListener("click",function () {
-   types.forEach(img => {
-      img.classList.remove("active");
-   })
-   photos.forEach(ph => {
-      ph.classList.remove("active");
-   })
-   img.classList.add("active");
-   photos.forEach(ph => {
-      if (img.getAttribute("data-name") == ph.getAttribute("data-name"))
-      {
-         ph.classList.add("active");
-      }
-   })
+      types.forEach(img =>{
+         img.classList.remove("active")
+      })
+      img.classList.add("active")
+      photos.forEach(ph => {
+         ph.classList.remove("active")
+      })
+      photos.forEach(ph => {
+         if ( img.getAttribute("data-name") == ph.getAttribute("data-name") )
+         {
+            ph.classList.add("active")
+         }
+      })
    })
 })
-
-
-
-
-
